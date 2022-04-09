dc033ee377bdab5007c4bc3ed8b8eecab1e33ef7
OlegKuro <o.khayatov@skyeng.ru>
2022-04-10 01:03:23 +0300

    tuned admin a little bit

--- core/admin.py
+++ core/admin.py
@@ -6,11 +6,17 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (
+            ('Common info', {'fields': ('username',)}),
+            ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+            ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+            ('Important dates', {'fields': ('last_login', 'date_joined')}))
     )
     list_display = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
     search_fields = ('email', 'first_name', 'last_name', 'username')
+    exclude = ('password',)
+    readonly_fields = ('last_login', 'date_joined')
 
 
 admin.site.register(User, CustomUserAdmin)
