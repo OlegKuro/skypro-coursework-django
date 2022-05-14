589a1d6b543a0a8557428a2bee07067d143ee4da
OlegKuro <o.khayatov@skyeng.ru>
2022-05-14 03:07:44 +0300

    removed unused backend

--- typicalProj/auth_backends.py
+++ typicalProj/auth_backends.py
@@ -1,23 +1,4 @@
-from django.contrib.auth.backends import BaseBackend
 from rest_framework.authentication import SessionAuthentication
-
-from core.models import User
-
-class UsernamePasswordCookieBackend(BaseBackend):
-    def authenticate(self, request, username=None, password=None):
-        try:
-            user = User.objects.get(username=username)
-            if user.check_password(password):
-                return user
-            return None
-        except User.DoesNotExist:
-            return None
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
