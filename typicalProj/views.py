b1f96d9fb754328de8700fd0f25777bd5295f3b7
OlegKuro <o.khayatov@skyeng.ru>
2022-05-19 02:06:39 +0300

    settings

--- typicalProj/views.py
+++ typicalProj/views.py
@@ -14,10 +14,5 @@ NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 @psa(f'{NAMESPACE}:complete')
 def auth(request, backend):
     redirect: HttpResponseRedirect = do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
-    if iri_to_uri('api:8000') in redirect.headers['Location']:
-        redirect.headers['Location'] = redirect.headers['Location'].replace(
-            iri_to_uri('api:8000'),
-            iri_to_uri('okhayatov.ga')
-        )
 
     return redirect
