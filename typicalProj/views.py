391aff5f9bad0c46a0a13f45ff1872c3e4fb50fa
OlegKuro <o.khayatov@skyeng.ru>
2022-05-19 01:49:20 +0300

    test

--- typicalProj/views.py
+++ typicalProj/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.cache import never_cache
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
@@ -12,7 +12,8 @@ NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 @never_cache
 @psa(f'{NAMESPACE}:complete')
 def auth(request, backend):
-    redirect: HttpResponse = do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
+    redirect: HttpResponseRedirect = do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
+    raise Exception(redirect)
     for header in ['Location', 'location']:
         if 'api:8000' in redirect.headers[header]:
             redirect.headers[header] = redirect.headers[header].replace('api:8000', 'okhayatov.ga')
