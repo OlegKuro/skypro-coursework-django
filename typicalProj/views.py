a84eac2c220c9d844b294cca96617f353aca56eb
OlegKuro <o.khayatov@skyeng.ru>
2022-05-19 01:33:02 +0300

    mnkey patch get params (fix import)

--- typicalProj/views.py
+++ typicalProj/views.py
@@ -2,7 +2,6 @@
 from django.views.decorators.cache import never_cache
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.shortcuts import redirect
 
 from social_core.utils import setting_name
 from social_core.actions import do_auth
@@ -14,7 +13,7 @@ NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 @psa(f'{NAMESPACE}:complete')
 def auth(request, backend):
     if request.GET.get(REDIRECT_FIELD_NAME) is None:
-        return redirect('login-social-backend', **{
-            f'{REDIRECT_FIELD_NAME}': 'http://okhayatov.ga',
-        })
+        request.GET._mutable = True
+        request.GET[REDIRECT_FIELD_NAME] = 'http://okhayatov.ga'
+        request.GET._mutable = False
     return do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
