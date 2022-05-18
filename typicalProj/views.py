e94d94e9189959055fd2668b9ec75a34efbb5abf
OlegKuro <o.khayatov@skyeng.ru>
2022-05-19 01:56:27 +0300

    test

--- typicalProj/views.py
+++ typicalProj/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.cache import never_cache
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.utils.encoding import iri_to_uri
 
 from social_core.utils import setting_name
 from social_core.actions import do_auth
@@ -13,9 +14,10 @@ NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 @psa(f'{NAMESPACE}:complete')
 def auth(request, backend):
     redirect: HttpResponseRedirect = do_auth(request.backend, redirect_name=REDIRECT_FIELD_NAME)
-    raise Exception(redirect)
-    for header in ['Location', 'location']:
-        if 'api:8000' in redirect.headers[header]:
-            redirect.headers[header] = redirect.headers[header].replace('api:8000', 'okhayatov.ga')
+    if iri_to_uri('api:8000') in redirect.headers['Location']:
+        redirect.headers['Location'] = redirect.headers['Location'].replace(
+            iri_to_uri('api:8000'),
+            iri_to_uri('okhayatov.ga')
+        )
 
     return redirect
